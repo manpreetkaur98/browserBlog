359759bcdc0c1f9f2088243a20ea4a655099d2e7
Manpreet Kaur <mk4223165@gmail.com>
2020-09-26 10:20:24 -0400

    Change from http to https

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
-import "http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
+import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
 import aPages from "../pages/index.js";
 
 class Page {
